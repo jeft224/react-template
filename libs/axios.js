d4fd5805539621b1934c689a44b74118aefab32e
Abert <1248824453@qq.com>
2018-11-30 23:20:41 +0800

    modify

--- libs/axios.js
+++ libs/axios.js
@@ -13,3 +13,21 @@ Axios.interceptors.request.use(config => {
 },error => {
     
 })
+
+Axios.interceptors.response.use(response => {
+    if(response.data == undefined){
+        data = response.request.responseText
+    } else { 
+        data = response.data
+    }
+    return data;
+},error => {
+    if (error && error.response){
+        return Promise.reject(error)
+    }
+})
+
+
+export default function axios(){
+
+}
